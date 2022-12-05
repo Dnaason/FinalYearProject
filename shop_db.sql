20372566080107b704e235e1f9bb63b6691ce943
Dnaason <dnaason11@gmail.com>
2022-12-05 17:25:08 +0200

    update database

--- shop_db.sql
+++ shop_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 30, 2022 at 03:43 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: localhost
+-- Generation Time: Dec 05, 2022 at 04:23 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -52,6 +52,14 @@ CREATE TABLE `message` (
   `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
+(8, 32, 'naason', 'naason@gmail.com', '0783827435', 'hi put some thing here'),
+(9, 33, 'niyonkuru', 'niyo@gmail.com', '987654', 'hi how are u');
+
 -- --------------------------------------------------------
 
 --
@@ -72,6 +80,15 @@ CREATE TABLE `orders` (
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(12, 33, 'Muhire', '0783827435', 'muhire@gmail.com', 'cash on delivery', 'flat no. kicukiro kicukiro kicukiro kagarama Rwanda - 123445', ', imboga ( 1 )', 700, '04-Dec-2022', 'pending'),
+(13, 33, 'Muhire', '0783827435', 'muhire@gmail.com', 'cash on delivery', 'flat no. kicukiro kicukiro kicukiro kagarama Rwanda - 123445', ', Tomato ( 3 ), Apple ( 4 )', 1500, '05-Dec-2022', 'pending'),
+(14, 33, 'Zawadi', '0987654', 'zawadi@gmail.com', 'cash on delivery', 'flat no. Kigali kjh Kanome ga Rwanda - 5687', ', Apple ( 1 )', 300, '05-Dec-2022', 'completed');
+
 -- --------------------------------------------------------
 
 --
@@ -87,6 +104,15 @@ CREATE TABLE `products` (
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `category`, `details`, `price`, `image`) VALUES
+(26, 'Apple', 'fruits', 'Apple products are rich sources of folate, vitamin C, and potassium. Relative to phytonutrients', 300, 'apple.png'),
+(27, 'Banana', 'fruits', 'is very good for life', 1200, 'banana.png'),
+(28, 'Carrot', 'vegitables', '\r\nThey&#39;re rich in beta-carotene, a compound your body changes into vitamin A, which helps keep your eyes healthy.', 1500, 'carrot.png');
+
 -- --------------------------------------------------------
 
 --
@@ -101,6 +127,16 @@ CREATE TABLE `users` (
   `user_type` varchar(20) NOT NULL DEFAULT 'user',
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`, `image`) VALUES
+(31, 'admin', 'admin@gmail.com', 'admin', 'admin', ''),
+(32, 'naason', 'naason@gmail.com', 'ed735d55415bee976b771989be8f7005', 'admin', 'cabbage.png'),
+(33, 'Muhire', 'muhire@gmail.com', 'e9ca8852f856464e40f1065117e44991', 'user', 'pic-4.png'),
+(34, 'Mukunzi', 'yannick@gmail.com', '911f6332e7f90b94b87f15377263995c', 'user', 'pic-2.png');
 
 -- --------------------------------------------------------
 
@@ -165,37 +201,37 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
