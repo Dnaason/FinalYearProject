389d89528c0185934b5119cffab7e2dc576a7272
Dnaason <dnaason11@gmail.com>
2022-12-15 15:31:05 +0200

    update

--- farmer_header.php
+++ farmer_header.php
@@ -38,6 +38,7 @@ if(isset($message)){
          ?>
          <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
          <p><?= $fetch_profile['name']; ?></p>
+         <a href="farmer_update_profile.php" class="btn">update profile</a>
          <a href="logout.php" class="delete-btn">logout</a>
       </div>
 
